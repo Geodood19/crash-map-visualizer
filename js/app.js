0174427001209b4b2ca6cf2c10268298027de2f2
Geodood <jfth225@uky.edu>
2024-10-08 21:19:23 -0400

    added leaflet heat layer

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@
     maxZoom: 18,
     zoomControl: false,
     attributionControl: false,
-    latLngBounds: ([38.75, -85.0], [37.25, -84.0])
+    latLngBounds: ([38.75, -85.0], [37.25, -84.0]),
   };
 
   // create the Leaflet map
@@ -77,65 +77,85 @@
   // Might need to customize labels as it clashes with desired crash dots
 
   omnivore
-  .csv("data/Fayette_2023Crashes_KABCO.csv")
-  .on("ready", function (e) {
-    drawMap(e.target.toGeoJSON());
-  })
-  .on("error", function (e) {
-    console.log(e.error[0].message);
-  });
-// end of Papa.parse()
-
-
+    .csv("data/Fayette_2023Crashes_KABCO.csv")
+    .on("ready", function (e) {
+      drawMap(e.target.toGeoJSON());
+    })
+    .on("error", function (e) {
+      console.log(e.error[0].message);
+    });
+  // end of Papa.parse()
 
   function processData() {
     // loop through all the crash data
     // add it to the map via a simple marker point
     // Use styles to define how to draw the crash data
-    // need to also figure out how to programmatically access certain information about each crash, such as 
-        // if a pedestrian was struck (from DirAnalysisCode in the CSV)
+    // need to also figure out how to programmatically access certain information about each crash, such as
+    // if a pedestrian was struck (from DirAnalysisCode in the CSV)
     // need to sort out the collision date data outside of JavaScript (in Excel)
- 
   } // end processData()
 
   function drawMap(data) {
     // create Leaflet object with geometry data and add to map
     // geometry will pull from the Latitude and Longitude feature properties of data
+
+    // Extract lat/lng points for the heatmap layer
+    const heatPoints = data.features.map(function (feature) {
+      const lat = feature.geometry.coordinates[1]; // Latitude
+      const lng = feature.geometry.coordinates[0]; // Longitude
+
+      return [lat, lng]; // return lat lng for each crash
+    });
+
+    // Create the heatmap layer using Leaflet.heat
+    const heat = L.heatLayer(heatPoints, {
+      radius: 15,
+      blur: 15,
+      maxZoom: 17,
+      zIndex: 900,
+      gradient: {
+        0.4: "cyan",
+        0.55: "blue",
+        0.85: "red",
+        1.0: "yellow",
+      },
+    }).addTo(map); // Add the heat layer to the map
+
+    // define point map options
     const options = {
       pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
           radius: 5,
-          opacity: 1,
+          opacity: 0.4,
           weight: 1,
-          fillOpacity: 1,
+          fillOpacity: 0.25,
           fillColor: "#0063e4",
-          color: "#ddd"
+          color: "#ddd",
+          zIndex: "400",
         });
       },
     };
     // add the data to the map
     crashes = L.geoJson(data, options).addTo(map);
 
-    // fit the data to the extent of the crash data
-    map.fitBounds(crashes.getBounds(), {
-      padding: [20,20]
+    map.fitBounds(heat.getBounds(), {
+      padding: [20, 20],
     });
-    
+
+    //
   } // end drawMap()
 
   function updateMap() {
     // Loop through each crash data
     // update the data to visualize it how the user wishes when they click on the UI
     // updateMap should also update the legend and what is displayed in the legend, including crash statistics for the displayed feature type
-    // Need to also be able to showcase how to visualize the crashes based on attributes/properties defined in the processData function 
-        // that do not have their own field (pedestrian struck, crash with an animal, etc)
-    
+    // Need to also be able to showcase how to visualize the crashes based on attributes/properties defined in the processData function
+    // that do not have their own field (pedestrian struck, crash with an animal, etc)
   } // end updateMap
 
   function drawLegend() {
     // drawLegend adds data from the updateMap, drawMap functions and displays it
     // drawLegend should also have feature counts displayed in the legend
-
   } // end drawLegend()
 
   function createSliderUI() {
@@ -143,7 +163,6 @@
     // The slider UI will show the crashes that occurred in specific months with 13 slider positions possible
     // The first slider position will show ALL crashes (and a div element that shows which time series data is being shown)
     // Slider positions/ticks 2-13 will show individual crashes that occurred in January - December, respectively.
-
   } // end createSliderUI()
 
   function buttonUI() {
